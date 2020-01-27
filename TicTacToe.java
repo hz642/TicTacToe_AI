4a17479f9892a9cffc40a7375686c607741677f6
Sabeel Mansuri <smansuri@ucsd.edu>
2020-01-26 23:00:16 -0800

    Update skeleton

--- TicTacToe.java
+++ TicTacToe.java
@@ -43,6 +43,14 @@ class TicTacToe {
 
     /**
      * Returns winner or draw
+     * 
+     * TODO - This function should determine if one of the players
+     * has three X's or O's in a row, or if the game has ended in a draw.
+     * 
+     * If the X player has won, set winner = "X" and set run = false.
+     * If the O player has won, set winner = "O" and set run = false.
+     * If the game has ended in a draw, set winner = "draw" and set run = false.
+     * Otherwise, do not change winner or run.
      */
     private String winner() {
         return null;
@@ -50,6 +58,12 @@ class TicTacToe {
 
     /**
      * Performs move in game
+     * 
+     * TODO - This function should prompt the user to make a move. It will
+     * confirm that both the input and the requested move is valid. If either
+     * is untrue, it will repeatedly re-prompt the user until both conditions
+     * are met. After that, it will update gameBoard and emptySpaces. Finally,
+     * it will call changePlayer().
      */
     private void move() {
         return;
@@ -59,7 +73,11 @@ class TicTacToe {
      * Changes active player
      */
     private void changePlayer() {
-        return;
+        if (player.equals("X")) {
+            player = "O";
+        } else {
+            player = "X";
+        }
     }
 
     /**
@@ -85,6 +103,19 @@ class TicTacToe {
      * Ends game; option for reset
      */
     private void endGame() {
-        return;
+        String endMessage = "";
+        if (winner.equals("draw")) {
+            endMessage = "The game has ended in a draw.";
+        } else {
+            endMessage = "The " + winner + " player has won!";
+        }
+        System.out.println(endMessage);
+
+        run = false;
+        System.out.print("Want to play another game [y/n]? ");
+        String userSelection = input.next();
+        if(userSelection.equals("y")) {
+            run = true;
+        }  
     }
 }
